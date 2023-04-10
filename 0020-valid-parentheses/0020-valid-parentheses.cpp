955cb352557ae331b65e9edf7f91489c23513e6e
Abhishek Yadav <103676667+Abhishek9503@users.noreply.github.com>
2023-04-10 23:53:16 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,32 +1,61 @@
-class Solution {
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> st;
+        
+//         for(char ch:s) {
+//             if(st.empty() || ch == '(' || ch == '{' || ch == '[') {
+//                 st.push(ch);
+//                 continue;
+//             }
+            
+//             if(ch == ')') {
+//                 if(st.top() == '(')
+//                     st.pop();
+//                 else
+//                     return false;
+//             } else if(ch == '}') {
+//                 if(st.top() == '{')
+//                     st.pop();
+//                 else
+//                     return false;
+//             } else if(ch == ']') {
+//                 if(st.top() == '[')
+//                     st.pop();
+//                 else
+//                     return false;
+//             }
+//         }
+        
+//         return st.empty();
+        
+//        }
+// };
+     
+        
+        
+        // Second approach 
+        
+    class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+ stack<char> st;
         
         for(char ch:s) {
-            if(st.empty() || ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-                continue;
-            }
-            
-            if(ch == ')') {
-                if(st.top() == '(')
-                    st.pop();
-                else
-                    return false;
-            } else if(ch == '}') {
-                if(st.top() == '{')
-                    st.pop();
-                else
-                    return false;
-            } else if(ch == ']') {
-                if(st.top() == '[')
-                    st.pop();
-                else
-                    return false;
+            if (ch == '(')
+			    st.push(')');
+		    else if (ch == '{')
+			    st.push('}');
+            else if (ch == '[')
+                st.push(']');
+            else if (st.empty() || st.top() != ch)
+                return false;
+            else {
+                st.pop();
             }
         }
         
         return st.empty();
+        
     }
 };
