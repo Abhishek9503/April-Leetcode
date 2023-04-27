0dcb473eee06d8c82e4b8daa6397dc3e86b2e204
Abhishek Yadav <103676667+Abhishek9503@users.noreply.github.com>
2023-04-27 23:42:56 +0530

    Time: 3 ms (45.34%), Space: 5.9 MB (63.99%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-                  return sqrt(n);
+                  // return sqrt(n);
+        
+     
+        // ct_divisors(n);
+        // int ans = 0;
+        // int i = 1;
+        // for(i = 1; i*i<=n; i++) if(divs[i]%2 != 0) ans++;
+        // for(i; i<=n; i++) if(divs[i]%2 == 0) ans++;
+        // return ans;
+
+        //works
+        int ans = 0;
+        for(int i = 1; i*i<=n; i++){
+            ans++;
+        }
+        return ans;
+      
 
     }
 };
