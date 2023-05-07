be1f09579b977b96d2d8ef401125ecaae39c3060
Abhishek Yadav <103676667+Abhishek9503@users.noreply.github.com>
2023-05-07 11:02:53 +0530

    Added solution - LeetHub

---
+++ String Mirror - GFG/string-mirror.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    string stringMirror(string s){
+        int n = s.size();
+        int ind = 0;
+        for(int i = 1; i<n; i++){
+            if(s[0]==s[1])  break;
+            if(s[i]<=s[i-1]){
+                ind = i;
+            }
+            else break; 
+        }
+        string t = s.substr(0,ind+1);
+        string p = t;
+        reverse(p.begin(),p.end());
+        return t+p; 
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        Solution ob;
+        string res=ob.stringMirror(str);
+        cout<<res<<endl;
+    }
+}
+// } Driver Code Ends
